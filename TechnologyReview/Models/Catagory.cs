3e9271d76f3b82d0b9ae43912cb0dd8b2793d6da
jjenkins0505 <jenkinsjazmyne@gmail.com>
2017-05-05 10:43:11 -0400

    Initial Commit.

---
+++ TechnologyReview/Models/Catagory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TechnologyReview.Models
+{
+    public class Catagory
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        public string Phone { get; set; }
+        public string LapTop { get; set; }
+
+        [Display(Name = "Smart Watches")]
+        public string SmartWatches { get; set; }
+
+
+        //ICollection is more efficient when setting up a one to many relationship code first. type of collection (cousin to a list)
+        public virtual ICollection<Book> Books { get; set; }
+
+
+    }
+
+
+}
